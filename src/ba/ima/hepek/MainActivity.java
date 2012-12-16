99bffd590b23ba97e63bb0d8e4f4d50d725098ce
gondzo <gondzo@gmail.com>
2012-12-16 02:51:59 +0100

    splash screen and main activity

---
+++ src/ba/ima/hepek/MainActivity.java
@@ -0,0 +1,160 @@
+package ba.ima.hepek;
+
+
+
+import ba.kloc.hepek.R;
+import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.Parameters;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	private Button hepekButton; 
+	MediaPlayer mp;
+	int dot = 200;      // Length of a Morse Code "dot" in milliseconds
+	int dash = 500;     // Length of a Morse Code "dash" in milliseconds
+	int short_gap = 200;    // Length of Gap Between dots/dashes
+	int medium_gap = 500;   // Length of Gap Between Letters
+	int long_gap = 1000;    // Length of Gap Between Words
+	long[] pattern = {
+	    0,  // Start immediately
+	    dot, short_gap, dot, short_gap, dot,    // s
+	    medium_gap,
+	    dash, short_gap, dash, short_gap, dash, // o
+	    medium_gap,
+	    dot, short_gap, dot, short_gap, dot,    // s
+	    long_gap
+	};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.hepekButton = (Button)this.findViewById(R.id.hepekBtn);
+        this.hepekButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				
+				playSound(R.raw.penzioner);
+				Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+				v.vibrate(pattern, -1);
+				
+				boolean hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+                System.out.println("imal flash"+hasFlash);
+				if (hasFlash){
+                	new FlashThread(pattern).start();
+                }
+				
+			}
+        	
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    /**
+     * Manager of Sounds
+     */
+    protected void playSound(int uri) {
+
+        
+    	if (mp!=null){
+    		if (!mp.isPlaying()){
+    			mp = MediaPlayer.create(this, uri);
+    		}
+    	}else{
+    		mp = MediaPlayer.create(this, uri);
+    	}
+        if (!mp.isPlaying()){
+        	mp.start();
+        }
+    }
+
+    
+    
+    public class FlashThread extends Thread {
+    	
+    	long[] pattern;
+    	public FlashThread(long[] pattern){
+    		this.pattern=pattern;
+    	}
+        @Override
+        public void run() {
+        	Camera cam=null;
+            // Simulate a slow network
+            try {
+            	
+            	cam = Camera.open();     
+            	Parameters p = cam.getParameters();
+            	p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            	if (Build.MANUFACTURER.equalsIgnoreCase("samsung")){
+            		p.setFlashMode(Parameters.FLASH_MODE_ON);
+            	}
+            	cam.setParameters(p);
+            	System.out.println("pocinjem flash pattern");
+            	cam.startPreview();
+            	if (Build.MANUFACTURER.equalsIgnoreCase("samsung")){
+            		cam.autoFocus(new AutoFocusCallback() {
+                        public void onAutoFocus(boolean success, Camera camera) {
+                        }
+                    });
+            	}
+            	boolean on=true;
+            	for (long l:pattern){
+            		if (on){
+            			System.out.println("on"+l);
+            			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            			if (Build.MANUFACTURER.equalsIgnoreCase("samsung")){
+                    		p.setFlashMode(Parameters.FLASH_MODE_ON);
+                    	}
+                    	cam.setParameters(p);
+            			
+            			on=false;
+            		}else{
+            			System.out.println("off"+l);
+            			p.setFlashMode(Parameters.FLASH_MODE_OFF);
+            			cam.setParameters(p);
+            			on=true;
+            		}
+            		sleep(l);
+            	}
+              
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+             finally {
+            	 System.out.println("finaly");
+            	try{
+            		cam.stopPreview();
+            		cam.release();
+            	}
+            	catch(Exception e){
+            		System.out.println("camstop failed");
+            	}
+
+             }
+        
+      }
+    }
+}
+ 
---
+++ src/ba/ima/hepek/SplashScreen.java
@@ -0,0 +1,63 @@
+package ba.ima.hepek;
+
+import ba.kloc.hepek.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+ 
+/**
+ * Splash screen activity
+ */
+public class SplashScreen extends Activity {
+ 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        setContentView(R.layout.splash_screen);
+ 
+        Handler handler = new Handler();
+ 
+        // run a thread after 2 seconds to start the home screen
+        handler.postDelayed(new Runnable() {
+ 
+            @Override
+            public void run() {
+ 
+                // make sure we close the splash screen so the user won't come back when it presses back key
+ 
+                finish();
+                // start the home screen
+                
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+ 
+            }
+ 
+        }, 4000); // time in milliseconds (1 second = 1000 milliseconds) until the run() method will be called
+        new SoundThread().start();
+    }
+    
+    
+public class SoundThread extends Thread {
+    	
+    	
+        @Override
+        public void run() {
+        	try {
+				sleep(2000);
+			
+	        	MediaPlayer mp = MediaPlayer.create(SplashScreen.this, R.raw.junuz);
+	        	mp.start();
+        	} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        
+      }
+    }
+ 
+}
